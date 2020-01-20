fa829fce0fa9aead6e287abf4b6409551783fd68
Billi Ramirez <ramirezbilly42@outlook.com>
2020-01-20 14:42:12 -0600

    nodejs express server using typescript

---
+++ src/controllers/RootController.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import { get, controller, use } from "./decorators";
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  } else {
+    res.status(403);
+    res.send("Not permitted");
+  }
+}
+
+@controller("")
+class RootController {
+  @get("/")
+  getRoot(req: Request, res: Response) {
+    if (req.session && req.session.loggedIn) {
+      res.send(`
+        <div>
+          <div>You are logged int</div>
+          <a href="/auth/logout">Logout</a>
+        </div>
+      `);
+    } else {
+      res.send(`
+        <div>
+          <div>You are Not logged int</div>
+          <a href="/auth/login">Log In</a>
+        </div>
+      `);
+    }
+  }
+
+  @get("/protected")
+  @use(requireAuth)
+  getProtected(req: Request, res: Response) {
+    res.send("Welcome to protected user, logged user");
+  }
+}
